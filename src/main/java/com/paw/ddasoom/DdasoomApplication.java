bf5bfeae1b1dd6bf02eaa0b0933fe4d1043fe256
jihoon-devstu <wlsgksvheh@gmail.com>
2026-06-29 10:29:08 +0900

    Project 생성 및 패키지 구조화

---
+++ src/main/java/com/paw/ddasoom/DdasoomApplication.java
@@ -0,0 +1,13 @@
+package com.paw.ddasoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdasoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdasoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paw/ddasoom/DdasoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paw.ddasoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdasoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
