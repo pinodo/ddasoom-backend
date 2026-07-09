e7cc778a4aac9a27b162a36764fd04222c1dd5a0
seojin <seojointly@gmail.com>
2026-07-09 16:58:24 +0900

    합치기

---
+++ src/main/java/com/paw/ddasoom/support/dto/request/NoticeUpdateRequest.java
@@ -0,0 +1,19 @@
+package com.paw.ddasoom.support.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class NoticeUpdateRequest {
+
+  @NotBlank(message = "제목은 필수입니다.")
+  @Size(max = 255, message = "제목은 255자를 초과할 수 없습니다.")
+  private String title;
+
+  @NotBlank(message = "내용은 필수입니다.")
+  private String content;
+
+}
--- src/main/java/com/paw/ddasoom/support/exception/SupportErrorCode.java
+++ src/main/java/com/paw/ddasoom/support/exception/SupportErrorCode.java
@@ -4,7 +4,12 @@ import org.springframework.http.HttpStatus;
 
 import com.paw.ddasoom.common.exception.ErrorCode;
 
-public enum ErrorCode implements ErrorCode {
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum SupportErrorCode implements ErrorCode {
 
   NOTICE_NOT_FOUND(HttpStatus.NOT_FOUND, "SUPPORT_001", "해당 공지사항을 찾을 수 없습니다.");
 
