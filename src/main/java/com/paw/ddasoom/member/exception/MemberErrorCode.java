7780877788a15e8b226913a6bf688656bcf3cd1d
jihoon-devstu <wlsgksvheh@gmail.com>
2026-07-10 14:51:11 +0900

    ♻️ Refactor : MemberErrorCode 관리자 에러코드 추가

--- src/main/java/com/paw/ddasoom/member/exception/MemberErrorCode.java
+++ src/main/java/com/paw/ddasoom/member/exception/MemberErrorCode.java
@@ -15,7 +15,9 @@ public enum MemberErrorCode implements ErrorCode{
   ALREADY_USER_ROLE(HttpStatus.BAD_REQUEST, "MEMBER_002", "이미 추가 정보 입력이 완료된 회원입니다."),
   ALREADY_DELETED_MEMBER(HttpStatus.BAD_REQUEST, "MEMBER_003", "이미 탈퇴 처리된 회원입니다."),
   PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "MEMBER_004", "현재 비밀번호가 일치하지 않습니다."),
-  SOCIAL_MEMBER_HAS_NO_PASSWORD(HttpStatus.BAD_REQUEST, "MEMBER_005", "소셜 로그인 회원은 비밀번호를 변경할 수 없습니다.");
+  SOCIAL_MEMBER_HAS_NO_PASSWORD(HttpStatus.BAD_REQUEST, "MEMBER_005", "소셜 로그인 회원은 비밀번호를 변경할 수 없습니다."),
+  NOT_DELETED_MEMBER(HttpStatus.BAD_REQUEST, "MEMBER_006", "탈퇴 상태가 아닌 회원입니다."),
+  CANNOT_WITHDRAW_ADMIN(HttpStatus.BAD_REQUEST, "MEMBER_007", "관리자 계정은 강제 탈퇴할 수 없습니다.");
 
   
   // 실제 상태(필드)는 Enum이 가집니다.
