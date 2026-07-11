63fb6a98d502f0b1895c204a9defb0cf358a980e
pinodo <palifarec@gmail.com>
2026-07-11 17:27:25 +0900

    🐛 fix: RestClient 빌드 오류 수정

--- src/main/java/com/paw/ddasoom/common/config/WebConfig.java
+++ src/main/java/com/paw/ddasoom/common/config/WebConfig.java
@@ -1,10 +1,12 @@
 package com.paw.ddasoom.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 
 import com.paw.ddasoom.common.security.CorsProperties;
 
@@ -35,13 +37,22 @@ public class WebConfig implements WebMvcConfigurer{
                 .maxAge(3600);
     }
 
+    /**
+     * RestClient 빌드
+     * @param baseUrl
+     * @return restclient
+     */
     @Bean
-    public RestClient restClient(){
-        return RestClient.builder()
-            .baseUrl("${api.base-url}")
-            .build();
-    }
+    public RestClient restClient(@Value("${api.base-url}") String baseUrl) {
+    // baseUrl 끝 슬래시 보존 (서비스명 뒤에 오퍼레이션이 이어붙도록)
+    String rootUrl = baseUrl.endsWith("/") ? baseUrl : baseUrl + "/";
 
+    // 생성자에 baseUrl을 직접 넘겨야 스킴/호스트가 유지됨
+    DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory(rootUrl);
+    factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.NONE); // serviceKey 이중 인코딩 방지
 
-
+    return RestClient.builder()
+            .uriBuilderFactory(factory)   // baseUrl을 아는 팩토리
+            .build();                     // 여기선 .baseUrl() 중복 호출 불필요
+}
 }
