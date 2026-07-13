d0ed85384437a5a2c2d092d3edc4854e40626e95
jihoon-devstu <wlsgksvheh@gmail.com>
2026-07-13 10:57:40 +0900

    🐛 fix : SocialExtraInfoRequest - name 필드 정규식 추가

--- src/main/java/com/paw/ddasoom/member/dto/request/SocialExtraInfoRequest.java
+++ src/main/java/com/paw/ddasoom/member/dto/request/SocialExtraInfoRequest.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SocialExtraInfoRequest {
   @NotBlank(message = "이름은 필수입니다.")
+  @Pattern(regexp = "^[가-힣]+$|^[a-zA-Z\\s]+$", message = "이름은 한글 또는 영문만 입력할 수 있습니다.")
   private String name;
 
   @NotBlank(message = "닉네임은 필수입니다.")
