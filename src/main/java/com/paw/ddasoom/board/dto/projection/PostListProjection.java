3695b64ca5bfa3e2ddad976832caa8583b4f1523
horangdev1513 <horang1513@naver.com>
2026-07-16 14:46:55 +0900

    ♻️ refactor: contentPreview -> content로 수정

--- src/main/java/com/paw/ddasoom/board/dto/projection/PostListProjection.java
+++ src/main/java/com/paw/ddasoom/board/dto/projection/PostListProjection.java
@@ -25,7 +25,7 @@ public class PostListProjection {
     private final Long postId;
     private final String category;
     private final String title;
-    private final String contentPreview;
+    private final String content;
     private final int viewCount;
     private final int commentCount;
     private final LocalDateTime createdAt;
