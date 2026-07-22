c54134a8cfaed6fafd9790d52638e566105b374c
pinodo <palifarec@gmail.com>
2026-07-22 14:36:54 +0900

    ♻️ refactor: 주석 정리

--- src/main/java/com/paw/ddasoom/animal/batch/AnimalLikeSyncItem.java
+++ src/main/java/com/paw/ddasoom/animal/batch/AnimalLikeSyncItem.java
@@ -1,7 +1,7 @@
 package com.paw.ddasoom.animal.batch;
 
 /**
- * service 패키지 - dto/response 아님 (외부 경계가 아니라 내부 배치용)
+ * batch 패키지 - dto/response 아님 (외부 경계가 아니라 내부 배치용)
  * Redis dirty 해시 -> 파싱한 배치 대상
 */
 public record AnimalLikeSyncItem(
